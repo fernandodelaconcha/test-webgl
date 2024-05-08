5a664eca94bd846191ef65690c06a64adb40a600
Fernando de la Concha <fernandodelaconcha@gmail.com>
2024-05-08 12:09:40 +0200

    add basic shapes and camera controls

---
+++ main.js
@@ -0,0 +1,59 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setX(0);
+camera.position.setY(2);
+camera.position.setZ(-2);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.IcosahedronGeometry(1);
+const material = new THREE.MeshStandardMaterial({ color: 'yellow' });
+const icosahedron = new THREE.Mesh(geometry, material);
+
+scene.add(icosahedron);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  icosahedron.rotation.x += 0.01;
+  icosahedron.rotation.y += 0.005;
+  icosahedron.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
