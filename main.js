c4d23d23b71b0e686379811e9d9ae9fff460bb53
Fernando de la Concha <fernandodelaconcha@gmail.com>
2024-05-13 17:46:32 +0200

    add hex mesh generation

--- main.js
+++ main.js
@@ -1,59 +1,84 @@
 import './style.css';
 import * as THREE from 'three';
+import { BufferGeometryUtils, RGBELoader, SimplexNoise } from 'three/examples/jsm/Addons.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color("#FFEECC");
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
+camera.position.set(-17, 31, 33);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
+  antialias: true,
 });
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+renderer.setSize(innerWidth, innerHeight);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.physicallyCorrectLights = true;
 
-camera.position.setY(5);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.dampingFactor = 0.05;
+controls.enableDamping = true;
 
 renderer.render(scene, camera);
-
-const geometry = new THREE.IcosahedronGeometry(1);
-const material = new THREE.MeshStandardMaterial({ color: 'yellow' });
-const icosahedron = new THREE.Mesh(geometry, material);
-
-scene.add(icosahedron);
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(gridHelper);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+let hexagonGeometries = new THREE.BoxGeometry(0,0,0);
+let pmrem = new THREE.PMREMGenerator(renderer);
+let envmapTexture = await new RGBELoader().setDataType(THREE.FloatType).loadAsync("assets/envmap.hdr");
+let envmap = pmrem.fromEquirectangular(envmapTexture).texture;
 
-function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
+const simplex = new SimplexNoise();
+const MAX_HEIGHT = 10;
 
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
+for (let i = -15; i <= 15; i++) {
+  for (let j = -15; j <=15; j++) {
+    let position = new tileToPosition(i, j);
+
+    if (position.length() > 16) continue;
+    let noise = (simplex.noise(i * .1, j * .1) + 1) * .5;
+    noise = Math.pow(noise, 1.5);
+    makeHex(noise * MAX_HEIGHT, position);
+  }
 }
 
-document.body.onscroll = moveCamera;
-moveCamera();
+let hexMesh = new THREE.Mesh(
+  hexagonGeometries,
+  new THREE.MeshStandardMaterial({
+    envMap: envmap,
+    flatShading: true,
+  })
+)
+scene.add(hexMesh);
 
 function animate() {
   requestAnimationFrame(animate);
-
-  icosahedron.rotation.x += 0.01;
-  icosahedron.rotation.y += 0.005;
-  icosahedron.rotation.z += 0.01;
-
   controls.update();
-
   renderer.render(scene, camera);
 }
 
 animate();
+
+function hexGeometry(height, position) {
+  let geo = new THREE.CylinderGeometry(1, 1, height, 6, 1, false);
+  geo.translate(position.x, height * 0.5, position.y);
+
+  return geo;
+}
+
+function makeHex(height, position) {
+  let geo = hexGeometry(height, position);
+  hexagonGeometries = BufferGeometryUtils.mergeGeometries([hexagonGeometries, geo]);
+}
+
+function tileToPosition(tileX, tileY) {
+  return new THREE.Vector2((tileX + (tileY % 2) * .5) * 1.77, tileY * 1.535)
+}
